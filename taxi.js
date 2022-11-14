3a4b6b3d00d0f17c6d8f2c8e974fd4f46c41ad4f
Tom Tamen <tomtamen@gmx.fr>
2022-11-14 14:45:13 +0100

    ex 1 taxi

---
+++ taxi.js
@@ -0,0 +1,27 @@
+class character{
+    constructor(name,sanity){
+        this.name =name
+        this.sanity = sanity
+    }
+}
+
+let john = new character("John", 10);
+
+const songs =["Annisa - Wejdene","Die - Gazo", "Apocalypse 894 - Stupeflip","Xenogenesis - TheFatRat","Wavin' Flag - K'NAAN"]
+
+let taxi=0
+
+for(let traffic_light = 30; traffic_light>=0; traffic_light--){
+    let song = songs[Math.floor(Math.random() * songs.length)];
+    console.log('The current song is : '+song+"\n"+traffic_light+" x red lights remain.");
+    if(song === "Annisa - Wejdene"){
+        john.sanity--
+        taxi++
+    }
+    if(john.sanity ===0){
+        console.log("explosion")
+        break
+    }else if(traffic_light===0 && john.sanity >0){
+        console.log("John did it with "+taxi+" changes !")
+    }
+}
